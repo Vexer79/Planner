363580abb15a530c52640d95f27e1138526a0629
Vexer79 <rokytskyi.i@gmail.com>
2024-02-25 18:26:43 +0200

    feat: add task prototype

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const notStartedTasksContainer = document.getElementById("not-started-tasks-container");
+const notStartedTasksTemplate = document.getElementById("not-started-tasks-template");
+
+const inProcessTasksContainer = document.getElementById("in-process-tasks-container");
+const inProcessTasksTemplate = document.getElementById("in-process-tasks-template");
+
+const completedTasksContainer = document.getElementById("completed-tasks-container");
+const completedTasksTemplate = document.getElementById("completed-tasks-template");
+
+notStartedTasksContainer.appendChild(notStartedTasksTemplate.content.cloneNode(true));
+inProcessTasksContainer.appendChild(inProcessTasksTemplate.content.cloneNode(true));
+completedTasksContainer.appendChild(completedTasksTemplate.content.cloneNode(true));
