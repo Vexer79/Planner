f1d0f8567b73d1bc422562583d21b14787fa91d5
Vexer79 <rokytskyi.i@gmail.com>
2024-03-08 01:10:37 +0200

    refactor: improve efficiency drag-and-drop mobile algorithm

--- js/task.js
+++ js/task.js
@@ -1,13 +1,13 @@
 function drag(containers) {
     return function (event) {
         let selected = event.target;
-        for (let i = 0; i < containers.length; i++) {
-            containers[i].addEventListener("dragover", function (event) {
+        for (let container of containers) {
+            container.addEventListener("dragover", function (event) {
                 event.preventDefault();
             });
-            containers[i].addEventListener("drop", function (event) {
+            container.addEventListener("drop", function () {
                 if (selected !== null) {
-                    containers[i].appendChild(selected);
+                    container.appendChild(selected);
                 }
                 selected = null;
             });
@@ -18,24 +18,23 @@ function drag(containers) {
 function touch(containers) {
     return function (event) {
         let selected = event.target;
-        if(selected.className !== "task"){
-            selected = event.target.parentElement;
+        if (selected.className !== "task") {
+            selected = selected.parentElement;
         }
-        for (let i = 0; i < containers.length; i++) {
-            let pos = new Object();
-            for (let j = 0; j < containers.length; j++) {
-                pos[j] = containers[j].getBoundingClientRect();
-            }
-            containers[i].addEventListener("touchend", function (event) {
+        for (let dragContainer of containers) {
+            dragContainer.addEventListener("touchend", function (event) {
                 let lastPosX = event.changedTouches[event.changedTouches.length - 1].clientX;
                 let lastPosY = event.changedTouches[event.changedTouches.length - 1].clientY;
-                for (let i = 0; i < containers.length; i++) {
-                    if (pos[i].top < lastPosY && lastPosY < pos[i].bottom
-                        && pos[i].left < lastPosX && lastPosX < pos[i].right) {
-                        if (selected !== null) {
-                            containers[i].appendChild(selected);
+                for (let dropContainer of containers) {
+                    if (dragContainer !== dropContainer) {
+                        let pos = dropContainer.getBoundingClientRect();
+                        if (pos.top < lastPosY && lastPosY < pos.bottom
+                            && pos.left < lastPosX && lastPosX < pos.right) {
+                            if (selected !== null) {
+                                dropContainer.appendChild(selected);
+                            }
+                            selected = null;
                         }
-                        selected = null;
                     }
                 }
             });
