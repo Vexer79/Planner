182987dbee69234f192b783e5f1ed19bf66e9569
Vexer79 <rokytskyi.i@gmail.com>
2024-03-25 19:28:16 +0200

    refactor: change structure of task.js

--- js/task.js
+++ js/task.js
@@ -5,81 +5,48 @@
     const inProcessTasksTemplate = document.getElementById("in-process-tasks-template");
     const completedTasksTemplate = document.getElementById("completed-tasks-template");
 
-    function drag(event) {
-        let selected = event.target;
-        const currentTaskSection = document.querySelector(".active").textContent;
-        const parentContainer = this.parentElement;
-        for (let container of containers) {
-            container.addEventListener("dragover", function (event) {
-                event.preventDefault();
-            });
-            container.addEventListener("drop", function () {
-                if (selected !== null) {
-                    container.appendChild(selected);
-                    setCurrentTasksOfContanerToTasks(container, currentTaskSection);
-                    setCurrentTasksOfContanerToTasks(parentContainer, currentTaskSection);
-                    selected = null;
-                }
-            });
+    Task.create = {
+        notStarted: function (taskContent) {
+            createTask(notStartedTasksTemplate, containers[0], taskContent);
+        },
+        inProcess: function (taskContent) {
+            createTask(inProcessTasksTemplate, containers[1], taskContent);
+        },
+        completed: function (taskContent) {
+            createTask(completedTasksTemplate, containers[2], taskContent);
+        },
+    };
+
+    function createTask(template, container, taskContent) {
+        if (taskContent) {
+            const task = template.content.cloneNode(true);
+            task.children[0].children[0].textContent = taskContent;
+            task.children[0].addEventListener("dragstart", DragAndDrop.drag);
+            task.children[0].addEventListener("touchstart", DragAndDrop.touch);
+            container.appendChild(task);
         }
     }
 
-    function setCurrentTasksOfContanerToTasks(container, currentTaskSection) {
+    Task.setGlobalObject = function (object) {
+        Task.getObjectReference = Parser.getObjectFromJSON(object);
+    };
+
+    Task.setCurrentTasksOfContanerToTasks = function (container, currentTaskSection) {
         const containerObject = {};
         for (let i = 1; i < container.children.length; i++) {
             containerObject[i] = container.children[i].innerText;
         }
-        const containerId = getId(container);
-        Tasks.getObjectReference[currentTaskSection][containerId] = containerObject;
+        const containerId = getContainerId(container);
+        Task.getObjectReference[currentTaskSection][containerId] = containerObject;
         return containerObject;
-    }
+    };
 
-    function getId(element) {
+    function getContainerId(element) {
         return element.id
             .replace("-tasks-container", "")
             .split("-")
             .join(" ")
             .replace(/\s\w/, (symb) => symb.trim().toUpperCase());
-    }
-
-    function touch(event) {
-        let selected = event.target;
-        const currentTaskSection = document.querySelector(".active").textContent;
-        if (selected.className !== "task") {
-            selected = selected.parentElement;
-        }
-        const parentContainer = selected.parentElement;
-        document.body.style.overflow = "hidden";
-        selected.parentNode.style.overflow = "hidden";
-        for (let dragContainer of containers) {
-            dragContainer.addEventListener("touchend", function (event) {
-                let lastPosX = event.changedTouches[event.changedTouches.length - 1].clientX;
-                let lastPosY = event.changedTouches[event.changedTouches.length - 1].clientY;
-                for (let dropContainer of containers) {
-                    if (dragContainer !== dropContainer) {
-                        let pos = dropContainer.getBoundingClientRect();
-                        if (
-                            pos.top < lastPosY &&
-                            lastPosY < pos.bottom &&
-                            pos.left < lastPosX &&
-                            lastPosX < pos.right
-                        ) {
-                            if (selected !== null) {
-                                document.body.style.overflow = "auto";
-                                selected.parentNode.style.overflow = "auto";
-                                dropContainer.appendChild(selected);
-                                setCurrentTasksOfContanerToTasks(dropContainer, currentTaskSection);
-                                setCurrentTasksOfContanerToTasks(
-                                    parentContainer,
-                                    currentTaskSection
-                                );
-                                selected = null;
-                            }
-                        }
-                    }
-                }
-            });
-        }
     }
 
     Task.createUsingInterface = function () {
@@ -97,28 +64,6 @@
         taskContent.clear();
     };
 
-    function createTask(template, container, taskContent) {
-        if (taskContent) {
-            const task = template.content.cloneNode(true);
-            task.children[0].children[0].textContent = taskContent;
-            task.children[0].addEventListener("dragstart", drag);
-            task.children[0].addEventListener("touchstart", touch);
-            container.appendChild(task);
-        }
-    }
-
-    Task.create = {
-        notStarted: function (taskContent) {
-            createTask(notStartedTasksTemplate, containers[0], taskContent);
-        },
-        inProcess: function (taskContent) {
-            createTask(inProcessTasksTemplate, containers[1], taskContent);
-        },
-        completed: function (taskContent) {
-            createTask(completedTasksTemplate, containers[2], taskContent);
-        },
-    };
-
     Task.clearAll = function () {
         for (let container of containers) {
             let template = container.children[0];
@@ -126,5 +71,6 @@
             container.appendChild(template);
         }
     };
+
     window.Task = Task;
 })(window);
