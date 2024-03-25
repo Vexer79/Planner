5c5faf41147ebbb34b45ac4770e66fe4404ec64a
Vexer79 <rokytskyi.i@gmail.com>
2024-03-25 19:31:05 +0200

    fix: add required argument

--- js/HTTP-client.js
+++ js/HTTP-client.js
@@ -1,3 +1,3 @@
 (function (){
-    ajaxUtils.sendGetRequest("http://192.168.0.107:5500/js/tasks.json", Tasks.setGlobalObject, false);
+    ajaxUtils.sendGetRequest("http://192.168.0.107:5500/js/tasks.json", Task.setGlobalObject, false);
 })();
