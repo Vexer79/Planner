4e4824b1f08482d5b0b6d820f0e78b115a0691fc
Vexer79 <rokytskyi.i@gmail.com>
2024-03-30 22:23:45 +0200

    build: change requests ip

--- js/HTTP-client.js
+++ js/HTTP-client.js
@@ -1,3 +1,3 @@
 (function (){
-    ajaxUtils.sendGetRequest("http://192.168.0.114:5500/js/tasks.json", Task.setGlobalObject, false);
+    ajaxUtils.sendGetRequest("http://192.168.0.105:5500/js/tasks.json", Task.setGlobalObject, false);
 })();
