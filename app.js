833a26760ff75ce8642aa9d7a35ff2d0656e8c96
Vexer79 <rokytskyi.i@gmail.com>
2024-03-31 18:07:36 +0300

    feat: import user routes to server

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+const userRoutes = require("./routes/user");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(userRoutes);
+
+app.use("/", (req, res) => {
+    res.status(404).sendFile(__dirname, "views", "404.html");
 });
 
 app.listen(port, () => {
