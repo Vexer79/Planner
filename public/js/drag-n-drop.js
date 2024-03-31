74501a39053d7e59d55ec787e66801a1161fdac5
Vexer79 <rokytskyi.i@gmail.com>
2024-03-31 18:09:09 +0300

    feat: implement drag-n-drop using Sortable

--- public/js/drag-n-drop.js
+++ public/js/drag-n-drop.js
@@ -1,10 +1,9 @@
-import Sortable from "sortablejs";
 (function () {
     const containers = document.querySelectorAll(".task-body");
 
     for (const container of containers) {
         new Sortable(container, {
-            group: "shared", // set both lists to same group
+            group: "shared",
             animation: 150,
         });
     }
