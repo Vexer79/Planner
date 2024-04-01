29cec55c244aef432ce4533b3f5a1f12857a11bf
Vexer79 <rokytskyi.i@gmail.com>
2024-04-02 01:09:59 +0300

    fix: add path join method to send 404 page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 const userRoutes = require("./routes/user");
 
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(userRoutes);
 
 app.use("/", (req, res) => {
-    res.status(404).sendFile(__dirname, "views", "404.html");
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(port, () => {
