748dc734cc940c5060130d43553452d1da663273
Vexer79 <rokytskyi.i@gmail.com>
2024-04-02 23:43:35 +0300

    refactor: replace onEnd handler function to HTTP-client.js

--- public/js/HTTP-client.js
+++ public/js/HTTP-client.js
@@ -1,3 +1,27 @@
-(function (){
+(function (global){
     ajaxUtils.sendGetRequest("http://localhost:3000/tasks", Task.setGlobalObject, false);
-})();
+
+    let requests = {};
+
+    requests.changeTaskOrder = function (event) {
+        var itemEl = event.item;
+        console.log(itemEl, "onEnd");
+        console.log(event);
+        try {
+            console.log(`---Call save function---`);
+            console.log(`from container ${event.from}`);
+            console.log(`to container ${event.to}`);
+            console.log(`old index ${event.oldIndex}`);
+            console.log(`new index ${event.newIndex}`);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    requests.createTask = function(){
+        //make request to create task
+        //on complete -> create task
+    }
+
+    global.requests = requests;
+})(window);
--- public/js/drag-n-drop.js
+++ public/js/drag-n-drop.js
@@ -5,21 +5,7 @@
         new Sortable(container, {
             group: "shared",
             animation: 150,
-
-            onEnd: function (event) {
-                var itemEl = event.item;
-                console.log(itemEl, "onEnd");
-                console.log(event);
-                try {
-                    console.log(`---Call save function---`);
-                    console.log(`from container ${event.from}`);
-                    console.log(`to container ${event.to}`);
-                    console.log(`old index ${event.oldIndex}`);
-                    console.log(`new index ${event.newIndex}`);
-                } catch (error) {
-                    console.log(error);
-                }
-            }
+            onEnd: requests.changeTaskOrder,
         });
     }
 })();
