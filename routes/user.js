64d99e92baaf681f5ac8bf559f641c61fa911677
Vexer79 <rokytskyi.i@gmail.com>
2024-04-03 23:21:11 +0300

    feat: separate one big request to smaller

--- routes/user.js
+++ routes/user.js
@@ -2,18 +2,27 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-router.get("/tasks", (req, res) => {
+router.get("/day", (req, res) => {
     res.json({
-        D: {
-            notStarted: { 1: "day", 2: "hi" },
-            inProcess: { 1: "testInProcess" },
-            completed: { 1: "test" },
-        },
-        W: { notStarted: { 1: "week", 2: "hi" }, inProcess: {}, completed: {} },
-        M: { notStarted: { 1: "month", 2: "hi" }, inProcess: {}, completed: {} },
-        Y: { notStarted: { 1: "year", 2: "hi" }, inProcess: {}, completed: {} },
+        notStarted: { 1: "day", 2: "hi" },
+        inProcess: { 1: "testInProcess" },
+        completed: { 1: "test" },
     });
 });
+router.get("/week", (req, res) => {
+    res.json({ notStarted: { 1: "week", 2: "hi" }, inProcess: {}, completed: {} });
+});
+router.get("/month", (req, res) => {
+    res.json({ notStarted: { 1: "month", 2: "hi" }, inProcess: {}, completed: {} });
+});
+router.get("/year", (req, res) => {
+    res.json({ notStarted: { 1: "year", 2: "hi" }, inProcess: {}, completed: {} });
+});
+
+router.post("/day", (req, res) => {});
+router.post("/week", (req, res) => {});
+router.post("/month", (req, res) => {});
+router.post("/year", (req, res) => {});
 
 router.get("/", (req, res) => {
     res.type(".html").sendFile(path.join(__dirname, "public", "index.html"));
