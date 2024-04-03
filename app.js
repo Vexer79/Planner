c1d64c7dbc7ec59a4c54cbcb42f02a8f90024754
Vexer79 <rokytskyi.i@gmail.com>
2024-04-04 01:51:47 +0300

    build: import body parser

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const path = require("path");
+
 const express = require("express");
+const bodyParser = require('body-parser');
+
+const errorController = require("./controllers/errorController");
+
 const app = express();
 const port =  process.env.PORT || 3000;
 
 const rootDirectory = require("./util/path");
-
-const errorController = require("./controllers/errorController");
 const userRoutes = require("./routes/user");
 
 app.use(express.static(path.join(rootDirectory, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(userRoutes);
 
