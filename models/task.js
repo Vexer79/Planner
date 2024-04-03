78300c5ea28de51273096be1a2356be9bbe8ab29
Vexer79 <rokytskyi.i@gmail.com>
2024-04-04 01:51:16 +0300

    feat: implement save and fetch methods in Task class

--- models/task.js
+++ models/task.js
@@ -1,5 +1,27 @@
-module.exports = class Task{
-    constructor(){
-        
+const notStartedTasks = [];
+const inProcessTasks = [];
+const completedTasks = [];
+
+module.exports = class Task {
+    constructor(content, colour, startTime, completeTime, notifications, container, index) {
+        this.content = content;
+        this.colour = colour;
+        this.startTime = startTime;
+        this.completeTime = completeTime;
+        this.notifications = notifications;
+        this.container = container;
+        this.index = index;
     }
-}
+
+    save() {
+        notStartedTasks.push(this);
+    }
+
+    static fetchAll() {
+        return {
+            notStarted: { ...notStartedTasks },
+            inProcess: { ...inProcessTasks },
+            completed: { ...completedTasks },
+        };
+    }
+};
