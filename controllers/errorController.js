ab670dd90f44b802682948dc8860e165ccba450c
Vexer79 <rokytskyi.i@gmail.com>
2024-04-04 00:37:36 +0300

    fix: change require path

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const rootDirectory = require("./util/path");
+const rootDirectory = require("../util/path");
 
 exports.get404Page = (req, res) => {
     res.status(404).sendFile(path.join(rootDirectory, "views", "404.html"));
